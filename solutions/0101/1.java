32d73fd76047bcf786cb16608b1ea96d7a96acc3
William Liu <nettee.liu@gmail.com>
2022-12-01 18:58:06 +0800

    101

---
+++ solutions/0101/1.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        } else {
+            return isSymmetric(root.left, root.right);
+        }
+    }
+
+    private boolean isSymmetric(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) {
+            return true;
+        } else if (t1 == null || t2 == null) {
+            return false;
+        } else {
+            return t1.val == t2.val
+                && isSymmetric(t1.left, t2.right)
+                && isSymmetric(t1.right, t2.left);
+        }
+    }
+}
